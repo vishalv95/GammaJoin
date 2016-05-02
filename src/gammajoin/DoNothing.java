96d0a6258a558c924be0402d1e219324d53c1b5f
vishalv95 <vishalv95@gmail.com>
2016-05-02 00:11:52 -0500

    Added DoNothing and Sink

--- src/gammajoin/DoNothing.java
+++ src/gammajoin/DoNothing.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gammajoin;
 
-/**
- *
- * @author Vishal
- */
+import java.io.*;
+import gammaSupport.*;
+
 public class DoNothing extends Thread{
+    BufferedReader in; 
+    PrintStream out;
+    public DoNothing(BufferedReader in, PrintStream out){
+        this.in = in;
+        this.out = out;
+    }
     
+    public void run(){
+        String input;
+        try{
+            while(true){
+                input = in.readLine();
+                if(input == null) break;
+                out.println(input);
+                out.flush();
+            }
+            out.flush();
+            out.close();
+        }
+        
+        catch (IOException e) {
+            ReportError.msg(this.getClass().getName() + e);
+        }
+    }
 }
--- src/gammajoin/Sink.java
+++ src/gammajoin/Sink.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gammajoin;
 
-/**
- *
- * @author Vishal
- */
+import gammaSupport.ReportError;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+
 public class Sink extends Thread {
+    BufferedReader in;
+    public Sink(BufferedReader in){
+        this.in = in;
+    }
     
+    public void run(){
+        String input;
+        try{
+            while(true){
+                input = in.readLine();
+                if(input == null) break;
+            }
+        }
+        
+        catch (IOException e) {
+            ReportError.msg(this.getClass().getName() + e);
+        }
+    }
 }
